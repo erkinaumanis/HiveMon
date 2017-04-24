022a0e4875678ccb3533f4b499c0a4fe2992dde9
ches <ches@cheswick.com>
2017-04-24 13:10:31 -0400

    adding more boilerplate

---
+++ HiveMon/Defines.h
@@ -0,0 +1,31 @@
+//
+//  Defines.h
+//  HiveMon
+//
+//  Created by ches on 17/4/24.
+//  Copyright © 2017 Cheswick.com. All rights reserved.
+//
+
+#ifndef Defines_h
+#define Defines_h
+
+
+#define USED(x) ((void)(x))
+
+// view tools
+
+#define BELOW(r)    ((r).origin.y + (r).size.height)
+#define RIGHT(r)    ((r).origin.x + (r).size.width)
+
+#define SET_VIEW_X(v,nx) {CGRect f = (v).frame; f.origin.x = (nx); (v).frame = f;}
+#define SET_VIEW_Y(v,ny) {CGRect f = (v).frame; f.origin.y = (ny); (v).frame = f;}
+
+#define SET_VIEW_WIDTH(v,w)     {CGRect f = (v).frame; f.size.width = (w); (v).frame = f;}
+#define SET_VIEW_HEIGHT(v,h)    {CGRect f = (v).frame; f.size.height = (h); (v).frame = f;}
+
+#define CENTER_VIEW(cv, v)  {CGRect f = (cv).frame; \
+f.origin.x = ((v).frame.size.width - f.size.width)/2.0; \
+(cv).frame = f;}
+
+
+#endif /* Defines_h */
--- HiveMon/DevicesVC.m
+++ HiveMon/DevicesVC.m
@@ -6,6 +6,7 @@
 //  Copyright © 2017 Cheswick.com. All rights reserved.
 //
 
+#import "Defines.h"
 #import "DevicesVC.h"
 
 @interface DevicesVC ()
@@ -14,15 +15,140 @@
 
 @implementation DevicesVC
 
+- (id)initWithStyle:(UITableViewStyle)style {
+    self = [super initWithStyle:style];
+    if (self) {
+    }
+    return self;
+}
+
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    self.title = @"Devices";
+
 }
 
 
+- (void) viewWillAppear:(BOOL)animated {
+    [super viewWillAppear:animated];
+    
+    [self.navigationController.navigationBar setOpaque:YES];
+    [self.navigationController.navigationBar setTranslucent:NO];
+    self.navigationController.toolbar.backgroundColor = [UIColor lightGrayColor];
+    self.navigationController.toolbar.opaque = YES;
+    self.navigationController.toolbar.translucent = NO;
+    self.navigationController.toolbarHidden = NO;
+    
+//    SET_VIEW_WIDTH(iCloudHeaderView, self.view.frame.size.width);
+//    SET_VIEW_WIDTH(localHeaderView, self.view.frame.size.width);
+}
+
+- (void) viewDidAppear:(BOOL)animated {
+    [super viewDidAppear:animated];
+    
+}
+
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+}
+
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation {
+    return YES;
+}
+#pragma mark - Table view data source
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    return 2;
+}
+
+- (CGFloat)tableView:(UITableView *)tableView heightForHeaderInSection:(NSInteger)section {
+    return 1;   // XXXX
+}
+
+#ifdef notdef
+- (UIView *)tableView:(UITableView *)tableView viewForHeaderInSection:(NSInteger)section {
+    switch (section) {
+        case EnvelopeInCloudSection:
+            return iCloudHeaderView;
+        case EnvelopeInLocalSection:
+            return localHeaderView;
+        default:
+            return nil;   // inconceivable
+    }
+}
+#endif
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return 0;   // XXXX
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    static NSString *CellIdentifier = @"EnvelopeCell";
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
+    if (cell == nil) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleSubtitle reuseIdentifier:CellIdentifier];
+    }
+#ifdef notyet                
+    cell.textLabel.text = localTitle;
+            cell.imageView.backgroundColor = [UIColor yellowColor];
+                 cell.userInteractionEnabled = YES;
+                cell.accessoryView = nil;
+                UIActivityIndicatorView *busyLoading = [[UIActivityIndicatorView alloc]
+                                                        initWithActivityIndicatorStyle:UIActivityIndicatorViewStyleGray];
+                CGRect f = cell.frame;
+                f.size.width = f.size.height;
+                f.origin.x = cell.frame.size.width - f.size.width;
+                busyLoading.frame = f;
+                busyLoading.hidesWhenStopped = YES;
+                cell.accessoryView = busyLoading;
+                return cell;
+#endif
+    
+    cell.textLabel.text = @"XXXX";
+    cell.textLabel.textAlignment = NSTextAlignmentLeft;
+    
+    cell.accessoryView = nil;
+    return cell;
+}
+
+- (BOOL)tableView:(UITableView *)tableView
+canMoveRowAtIndexPath:(NSIndexPath *)indexPath {
+    return YES;
+}
+
+- (void)tableView:(UITableView *)tableView
+commitEditingStyle:(UITableViewCellEditingStyle)editingStyle
+forRowAtIndexPath:(NSIndexPath *)indexPath {
+    
+    if (editingStyle == UITableViewCellEditingStyleDelete) {
+    } else if (editingStyle == UITableViewCellEditingStyleInsert) {
+        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+    }
+}
+
+- (void)tableView:(UITableView *)tableView
+moveRowAtIndexPath:(NSIndexPath *)fromIndexPath
+      toIndexPath:(NSIndexPath *)toIndexPath {
+    NSLog(@"move from %ld.%ld to %ld.%ld",
+          (long)fromIndexPath.section, (long)fromIndexPath.row,
+          (long)toIndexPath.section, (long)toIndexPath.row);
+    
+#ifdef notyet
+    switch (toIndexPath.section) {
+        case EnvelopeInLocalSection:
+            [localEnvelopes removeObjectAtIndex:fromIndexPath.row];
+            [localEnvelopes insertObject:e atIndex:toIndexPath.row];
+            break;
+        case EnvelopeInCloudSection:
+            [self moveToCloud:e.path];
+            [localEnvelopes removeObjectAtIndex:fromIndexPath.row];
+            // XXXX delete this row
+            // XXXX            [iCloudEnvelopes insertObject:e atIndex:toIndexPath.row];
+            break;
+    }
+#endif
 }
 
 
